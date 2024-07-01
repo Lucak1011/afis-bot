f95fcff686c9e17ddfb1c6e5f03329299775085a
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-07-01 20:47:58 +0100

    It might work

--- bot.py
+++ bot.py
@@ -1,2 +1,12 @@
-print("Hello world!")
-print("Works?")
+#bot.py
+import os
+import discord
+
+
+TOKEN = os.environ['MTI1NzM4MjQ1MzU4OTMxNTY0NA.Gmc9ZG.gFxGuZVPEc8mdGap_lDYCgJ7C0kGeKxVXURq-Y']
+client = discord.Client()
+from dotenv import load_dotenv
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to discord')
+client.run(TOKEN)
