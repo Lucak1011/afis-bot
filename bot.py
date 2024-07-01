77b6bc2bf73bed1d69a9f2da4a2ac855e44da1b0
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-07-01 20:52:31 +0100

    Revert "It might work"
    This reverts commit f95fcff686c9e17ddfb1c6e5f03329299775085a.

--- bot.py
+++ bot.py
@@ -1,12 +1,2 @@
-#bot.py
-import os
-import discord
-
-
-TOKEN = os.environ['MTI1NzM4MjQ1MzU4OTMxNTY0NA.Gmc9ZG.gFxGuZVPEc8mdGap_lDYCgJ7C0kGeKxVXURq-Y']
-client = discord.Client()
-from dotenv import load_dotenv
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to discord')
-client.run(TOKEN)
+print("Hello world!")
+print("Works?")
