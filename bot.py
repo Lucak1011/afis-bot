d9f0a8fc96ec54f47dbed6294874a4c1f8094a26
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-07-01 22:48:04 +0100

    It might work

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import discord
+from discord.app_commands import commands
 from dotenv import load_dotenv
 
 # Load the .env file with the token
@@ -8,12 +9,16 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Create a client instance
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
 
 # Define the event
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
 
 # Run the bot
 client.run(TOKEN)
