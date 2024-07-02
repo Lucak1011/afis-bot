8a7bb6f4b088a468fd464eb7bab31757691b5d29
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-07-02 22:50:59 +0100

    The final commit?

--- index.js
+++ index.js
@@ -36,10 +36,26 @@ for (const folder of commandFolders) {
 }
 
 //Recieves interactions
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
-});
 
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+});
 // Log in to Discord with your client's token
 client.login(token);
