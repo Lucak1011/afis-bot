cde54525c1f21b315b18ac8a4d8ffa6f0d69a3b9
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-07-04 20:46:40 +0100

    It works!!!

--- commands/utility/pingpong.js
+++ commands/utility/pingpong.js
@@ -1,11 +1,10 @@
-const { REST, Routes } = require('discord.js')
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with pong'),
+        .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('pong');
+        await interaction.reply('Pong!');
     },
 };
