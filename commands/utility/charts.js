17e43b8f429a33ee2507052b5cb7b0091e3c3f26
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-07-04 21:30:19 +0100

    Add Charts Command
    Adds chart command with option

---
+++ commands/utility/charts.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js')
+// options must be 1-32 chars and not contain caps
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('charts')
+        .setDescription('Provides a chartfox link for aerodrome charts')
+        .addStringOption(option =>
+            option.setName('icao')
+                .setDescription('Enter airport ICAO')
+                .setRequired(true)),
+    async execute(interaction) {
+        const icaoCode = interaction.options.getString('icao')
+        await interaction.reply(`https://chartfox.org/${icaoCode}`)
+    },
+
+};
