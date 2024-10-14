e9bdfcd89f895d4671ab47b937fefdb59a9d8989
Luca Kulaga <135963553+Lucak1011@users.noreply.github.com>
2024-10-14 21:32:53 +0100

    Update metarfetch.js

---
+++ commands/utility/metarfetch.js
@@ -0,0 +1,39 @@
+const {SlashCommandBuilder, REST, Routes} = require('discord.js');
+const request = require('request');
+
+
+//command
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('metar')
+    .setDescription('Pulls a METAR when an ICAO code is entered')
+    .addStringOption(option =>
+        option.setName('ICAO')
+        .setDescription('Enter an ICAO code')
+        .setRequired(true)),
+    async execute(interaction){
+        const icao = interaction.options.getString('ICAO').toUpperCase();
+
+        let options = {
+            'method': 'GET',
+            'url': `https://metar.vatsim.net/${icao}`,
+            'headers': {
+                'Accept': 'text/plain'
+            }
+        };
+
+        await interaction.deferReply();
+
+        request(options, (error, response) =>{
+            if (error) {
+                console.error(error);
+                interaction.editReply('There was an error fetching METAR data, try again later!');
+                return;
+            }
+
+            interaction.editReply(`METAR data for ${icao}: \n\`\`\`${response.body}\`\`\``)
+        });
+
+    }
+};
